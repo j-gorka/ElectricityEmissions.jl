0a3882f0528318f930a124ff43bba9910358921e
j-gorka <joe.gorka13@gmail.com>
2024-10-10 13:58:44 -0600

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.52

---
+++ src/ElectricityEmissions.jl
@@ -0,0 +1,5 @@
+module ElectricityEmissions
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ElectricityEmissions
+using Test
+
+@testset "ElectricityEmissions.jl" begin
+    # Write your tests here.
+end
